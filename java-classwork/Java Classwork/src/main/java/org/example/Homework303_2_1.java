df6b08ce7b3c9e852f8817a03917319c95784755
Leng Tran <leng.tran@live.com>
2024-05-02 09:11:18 -0500

    Updated homework.

--- java-classwork/Java Classwork/src/main/java/org/example/Homework303_2_1.java
+++ java-classwork/Java Classwork/src/main/java/org/example/Homework303_2_1.java
@@ -1,7 +1,9 @@
 package org.example;
 
-public class Homework303_2_1 {
+import java.text.DecimalFormat;
 
+public class Homework303_2_1 {
+    static final double SALES_TAX = 0.045;
     public static void main(String[] arg){
 
         // Write a program that declares two integer variables, assigns an integer to each, and adds them together.
@@ -46,8 +48,48 @@ public class Homework303_2_1 {
         System.out.println("The result of dividing " + num1 + " by " + num2 + " is: " + result);
         // Skipped casting
 
-        //
+        // Write a program that declares two integer variables, x and y, and assigns the number 5 to x and the number 6 to y.
+        // Declare a variable q and assign y/x to it and print q. Now, cast y to a double and assign it to q. Print q again.
 
+        int x = 5;
+        int y = 6;
+
+        int q = y/x;
+
+        System.out.println("After integer division: " + q);
+
+        q = (double) y;
+
+        System.out.println("After casting y to double: " + q);
+
+        // Write a program that declares a named constant and uses it in a calculation. Print the result.
+
+        final double PI = 3.141592653589793;
+
+        double radius = 5.0;
+        double area = PI * Math.pow(radius, 2);
+
+        System.out.println("The area of the circle is: " + area);
+
+
+        // Write a program where you create three variables that represent products at a cafe. The products could be beverages like coffee, cappuccino, espresso, green tea, etc.
+        // Assign prices to each product. Create two more variables called subtotal and totalSale and complete an “order” for three items of the first product, four items of the second product, and two items of the third product.
+        // Add them all together to calculate the subtotal. Create a constant called SALES_TAX and add sales tax to the subtotal to obtain the totalSale amount. Be sure to format the results to two decimal places.
+
+        double coffee = 2.5;
+        double cappuccino = 3.5;
+        double espresso = 4.5;
+
+        double subtotal = (coffee * 3) + (cappuccino * 4) + (espresso * 2);
+
+        double salesTaxAmount = subtotal * SALES_TAX;
+        double totalAmount = subtotal *salesTaxAmount;
+
+        DecimalFormat df = new DecimalFormat("0.##");
+
+        System.out.println("subtotal : " + df.format(subtotal));
+        System.out.println("Sales Tax : " + df.format(salesTaxAmount));
+        System.out.println("Total Sale : " + df.format(totalAmount));
 
 
     }
---
+++ java-classwork/Java Classwork/src/main/java/org/example/Tester.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Tester {
+    public static void main(String[] args) {
+        // Not for grading. 
+
+        //
+
+    }
+}
